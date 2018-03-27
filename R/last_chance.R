b88436777c1d22b39dd52172162f2ea2e2762074
richardjtelford <richard.telford@bio.uib.no>
2018-03-27 18:56:32 +0200

    initial commit

---
+++ R/last_chance.R
@@ -0,0 +1,22 @@
+#' Chironomid assemblages from Last Chance Lake.
+#'
+#' A dataset containing the ages, counts sums and chironomid assemblages from 22 samples from Last Chance Lake from Axford et al (2017).
+#' @docType data
+#' @name last_chance
+#' @aliases last_chance0
+#'
+#' @usage data(last_chance)
+#'
+#' @format A data frame with 22 rows and 28 variables:
+#' \describe{
+#'   \item{age_calBP}{Sample age}
+#'   \item{totcaps}{Count sum}
+#'   \item{Corynon}{First species}
+#'   ...
+#' }
+#' @source \url{https://www.ncdc.noaa.gov/paleo-search/study/22091}
+#' @references Axford, Y. et al. 2017. Timing and magnitude of early to middle Holocene warming in East Greenland inferred from chironomids. Boreas. doi: 10.1111/bor.12247 
+#' @examples
+#' data(last_chance)
+#' @keywords datasets
+NULL
