0ef96dff40c26d7bdfb3210ec5533f84ef9f8827
richardjtelford <richard.telford@bio.uib.no>
2019-05-02 18:13:57 +0200

    Help file filled out, assertions

--- R/est2_n.R
+++ R/est2_n.R
@@ -8,12 +8,29 @@
 #'@param nmin integer; possible count sum to start direct search at
 #'@param nmax integer; possible count sum to end direct seach at
 #'
+#'@details Estimates the count sum with two methods.
+#' The first estimate is 100/p_min where p_min is the mimumum percent.
+#' The second estimate is the value N within the range nmin:nmax for which all p/100 * N is an integer for all percent p. The proportion of taxa for which this is an integer is given by the score. A warning is given if the score is below 1. This can happen nmax is less than the count sum, or if the percent have been incorrectly calculated (perhaps an error in rounding.
+#' 
+#'  @return A nested tibble
+#'  #' \itemize{
+#'   \item ID_cols - one or more user specified columns identifying the assemblages 
+#'   \item data - nested raw data from each assemblage
+#'   \item direct_search - nested raw output of the direct_search method
+#'   \item direct_search_est - nested with the value(s) of \code{est_n_direct} with the highest \code{score}. If the score is <1 a warning is given. This can contain more than row, especially if the precision of the percent data is low and the estimated count sum is high.
+#'   \item minpc - the minimum percentage for each assemblage
+#'   \item est_n_minpc estimate count sum for the minimum percent method
+#'   \item est_min_minpc - lowest possible count sum given precision of the percentage data 
+#'   \item est_max_minpc - ditto for hightest possible count sum
+#'   \item n_taxa - number of taxa
+#' } 
+#'@references Telford (2019) Tools for identifying unexpectely low microfossil count sums. Preprint. 
 #'@examples 
 #'require("dplyr")
 #' data(last_chance)
 #' last_chance0  %>% 
 #'   tidyr::gather(key = taxon, value = percent, -age_calBP, -totcaps) %>% 
-#'   estimate_n(digits = 2, ID_cols = c("age_calBP", "age_calBP"))
+#'   estimate_n(digits = 2, ID_cols = c("age_calBP", "totcaps"))
 #' 
 #'@importFrom dplyr rename mutate group_by_at vars one_of n slice summarise
 #'@importFrom tidyr nest unnest
@@ -21,12 +38,15 @@
 #'@importFrom magrittr %>%
 #'@importFrom rlang !! set_names .data
 #'@importFrom utils data
+#'@importFrom assertr assert in_set just_warn within_bounds
 #'@export
 estimate_n <- function(x, percent_col = "percent", taxon_col = "taxon", ID_cols, digits = 2, nmin = 1L, nmax = 1000L){
 
   digits <- digits + 2 #analyse proportions
-  possible_n <- x %>%
+  possible_n <- x %>% 
     rename(.percent = !!percent_col, .taxon = !!taxon_col) %>%
+    assert(within_bounds(0, 100), .data$.percent) %>% #check percent within 0- 100
+    filter(.data$.percent > 0) %>% #remove any 0 values - give Inf estimates
     mutate(
       p = .data$.percent / 100,
       p_min = .data$p - 0.5 * 10 ^ -digits,
@@ -58,7 +78,8 @@ estimate_n <- function(x, percent_col = "percent", taxon_col = "taxon", ID_cols,
         .data$direct_search, 
         ~slice(., which.max(score):n()) %>% 
           slice(1:(which.min(score == max(score)) - 1)) %>% 
-          rename(est_n_direct = n)
+          select(est_n_direct = n, score) %>% 
+          assert(in_set(1), score, error_fun = just_warn)
           ),
       
       #minimum percent method
@@ -79,8 +100,5 @@ estimate_n <- function(x, percent_col = "percent", taxon_col = "taxon", ID_cols,
   return(possible_n)
 }
 
-# #test
-# tibble(sampleID = "a", count = c(5, 17, 36), percent = count/sum(count) * 100, taxon = letters[1:3]) %>% 
-#   mutate(percent = round(percent, 2)) %>%
-#   estimate_n2(digits = 2, ID_cols = "sampleID")
+
 
